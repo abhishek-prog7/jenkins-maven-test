1984f98280b89a567ad6947e9e52ad845c79c3fb
abhishek <abhishekchauhan10x@gmail.com>
2023-09-25 22:11:36 -0400

    Added changes for jenkins build failure

--- src/main/java/com/jenkins/maven/Main.java
+++ src/main/java/com/jenkins/maven/Main.java
@@ -3,5 +3,7 @@ package com.jenkins.maven;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is my jenkins project";
+
     }
 }
