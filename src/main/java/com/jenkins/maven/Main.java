0a6f0dcccd4ab4077e182676a687d988094e1b26
abhishek <abhishekchauhan10x@gmail.com>
2023-09-25 22:24:43 -0400

    testing CI

--- src/main/java/com/jenkins/maven/Main.java
+++ src/main/java/com/jenkins/maven/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is my jenkins project");
+        System.out.println("I am testing CI");
 
     }
 }
