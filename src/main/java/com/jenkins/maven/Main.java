b7f713768924bb7c77f323bc9d8254c28b8b63f8
abhishek <abhishekchauhan10x@gmail.com>
2023-09-28 22:51:35 -0400

    testing jenkins pipeline

--- src/main/java/com/jenkins/maven/Main.java
+++ src/main/java/com/jenkins/maven/Main.java
@@ -1,10 +1,14 @@
 package com.jenkins.maven;
 
+import com.learning.jenkins.messageSource;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is my jenkins project");
         System.out.println("I am testing CI");
+        messageSource message = new messageSource();
+        message.getMessage();
 
     }
 }
